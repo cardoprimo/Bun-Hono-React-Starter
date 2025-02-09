ee7c741e49a37ea1f9c9e95493d1391bcc4dc40f
DDKP <112834202+cardoprimo@users.noreply.github.com>
2025-02-09 11:52:10 +0100

    bun extra build config Update bun.config.ts

--- bun.config.ts
+++ bun.config.ts
@@ -1,16 +1,55 @@
-const result = await Bun.build({
-  minify: true,
-  target: 'bun',
-  format: 'esm',
-  splitting: true,
-  outdir: './build',
-  entrypoints: ['./index.ts', './frontend/index.ts],
-})
+import { BuildConfig, BuildOutput } from 'bun';
 
-if (!result.success) {
-  console.error('Build failed')
+const { NODE_ENV } = import.meta.env; // fix import
 
-  for (const message of result.logs) {
-    console.error(message)
+buildLog(
+  await Bun.build({
+    entrypoints: ['./index.ts', './frontend/index.ts],
+    format: 'esm',
+    outdir: 'dist',
+    target: 'browser',
+    ...extraConfig(),
+  }),
+);
+
+function extraConfig(): Partial<BuildConfig> {
+  switch (NODE_ENV ?? 'development') {
+    case 'development':
+      return {};
+    case 'production':
+      return { minify: true };
+    default:
+      console.log(
+        '\x1b[93m' +
+          `Unrecognizable "NODE_ENV": ${NODE_ENV}, is this a mistake?` +
+          '\x1b[0m',
+      );
+      return {};
+  }
+}
+
+function buildLog({ logs, outputs, success }: BuildOutput) {
+  if (success) {
+    for (const { path } of outputs) {
+      console.log(
+        '\x1b[32;2m' +
+          `Artifact generated successfully at "${path}"` +
+          '\x1b[0m',
+      );
+    }
+    console.log(
+      '\x1b[92;1m' +
+        `Build succeeded in ${NODE_ENV} mode` +
+        '\x1b[0m',
+    );
+  } else {
+    for (const log of logs) {
+      console.error(log);
+    }
+    console.error(
+      '\x1b[91;1m' +
+        `Build failed in ${NODE_ENV} mode` +
+        '\x1b[0m',
+    );
   }
 }
