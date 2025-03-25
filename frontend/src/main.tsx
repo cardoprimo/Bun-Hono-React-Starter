bf03c676520db4af679024eaa232614b3174faa4
cardoprimo <cardoprimo@proton.me>
2025-03-25 23:19:39 +0100

    extracting comp

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createRouter, RouterProvider } from '@tanstack/react-router';
+import {
+	createRouter as createTanStackRouter,
+	RouterProvider,
+} from '@tanstack/react-router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ClerkProvider, useAuth } from '@clerk/clerk-react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { ConvexReactClient } from 'convex/react';
+import { env } from '@server/env';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
@@ -14,7 +21,23 @@ import './index.css';
 const queryClient = new QueryClient(); // ?
 
 // Create a new router instance
-const router = createRouter({ routeTree, context: { queryClient } });
+// const router = createRouter({ routeTree, context: { queryClient } }); ç
+const router = routerWithQueryClient(
+	createTanStackRouter({
+		routeTree,
+		defaultPreload: 'intent',
+		defaultErrorComponent: DefaultCatchBoundary,
+		defaultNotFoundComponent: () => <NotFound />,
+		context: { queryClient },
+		Wrap: ({ children }) => (
+			<ConvexProvider client={convexQueryClient.convexClient}>
+				{children}
+			</ConvexProvider>
+		),
+		scrollRestoration: true,
+	}),
+	queryClient,
+);
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -22,11 +45,18 @@ declare module '@tanstack/react-router' {
 		router: typeof router;
 	}
 }
+const clerkPublicKey = env.PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const convex = new ConvexReactClient(env.CONVEX_URL);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
+		<ClerkProvider publishableKey={clerkPublicKey}>
+			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+				</QueryClientProvider>
+			</ConvexProviderWithClerk>
+		</ClerkProvider>
 	</React.StrictMode>,
 );
